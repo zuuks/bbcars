b2e781a55759038db3b1f910d86aa750b372b8e3
zuuks <zuksbre@gmail.com>
2024-11-24 03:33:19 +0100

    asdasd

--- modules/model-prodavnica.php
+++ modules/model-prodavnica.php
@@ -1,7 +1,27 @@
 <?php
+if (!$db) {
+    die("Greška pri povezivanju sa bazom.");
+}
+$markaDropdown = '<select name="marka">';
+$sql = "SELECT DISTINCT marka FROM vozila";
+$result = mysqli_query($db, $sql);
 
-
-$_page_view['view_filename'] = DIR_VIEW . 'view-prodavnica.php';
-
-
+if ($result && mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $marka = htmlspecialchars($row['marka']); 
+        $markaDropdown .= "<option value=\"$marka\">$marka</option>";
+    }
+    $markaDropdown .= '</select>';
+} else {
+    $markaDropdown = '<p>Trenutno nema dostupnih marki.</p>';
+}
 ?>
+<div class="prodavnica">
+    <div class="filteri">
+        <h1 class="h3Prod">Automobili</h1>
+        <?php 
+        echo $markaDropdown;
+        ?>
+        <span></span>
+    </div>
+</div>
--- template/view-prodavnica.php
+++ template/view-prodavnica.php
@@ -1,9 +1,4 @@
-<div class="prodavnica">
-    <div class="filteri">
-        <h1 class="h3Prod">Automobili</h1>
-        <select name="marka" id="marka" aria-placeholder="Sve marke" tabindex="-1">
-            
-        </select>
-        <span></span>
-    </div>
-</div>
+<?php
+include(DIR_MODULES . 'model-prodavnica.php');
+echo $markaDropdown;
+?>
