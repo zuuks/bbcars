8f777bb82893da5ad9d930117964cd779e8bb104
LazarStankovic03 <lazar.stankovic.221@singimail.rs>
2024-11-25 17:43:11 +0100

    Ciscenje koda

--- modules/model-admin-panel.php
+++ modules/model-admin-panel.php
@@ -1,5 +1,64 @@
 <?php
 $_page_view['view_filename'] = DIR_VIEW . 'view-admin-panel.php';
+require 'config.php';
+
+// Povezivanje sa bazom
+$conn = new mysqli(
+    $config['hostname'],
+    $config['username'],
+    $config['password'],
+    $config['db_name']
+);
+
+// Provera konekcije
+if ($conn->connect_error) {
+    die("Konekcija nije uspela: " . $conn->connect_error);
+}
+
+// Proverite da li je zahtev za izvoz
+if (isset($_GET['action']) && $_GET['action'] == 'export') {
+    // SQL upit za dobijanje svih automobila
+    $sql = "SELECT id, cena, marka, model, godiste, predjeni_kilometri FROM vozila";
+    $result = $conn->query($sql);
+
+    // Kreiranje niza za skladištenje podataka o vozilima
+    $vehicles = [];
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $vehicles[] = $row;
+        }
+
+        // Generisanje JSON-a
+        $json_data = json_encode($vehicles, JSON_PRETTY_PRINT);
+
+        // Postavljanje odgovarajućih zaglavlja za preuzimanje JSON fajla
+        header('Content-Type: application/json');
+        header('Content-Disposition: attachment; filename="vozila.json"');
+        
+        // Slanje JSON podataka za preuzimanje
+        echo $json_data;
+
+        exit; // Završava izvršavanje skripte nakon slanja fajla
+    } else {
+        echo "Nema podataka za eksportovanje."; // Ispis greške ako nema podataka
+    }
+}
 
 
+// Definisanje broja stavki po stranici i trenutne stranice
+$items_per_page = 20;
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$start_from = ($page - 1) * $items_per_page;
+
+// SQL upit za dobijanje vozila sa paginacijom
+$sql = "SELECT id, cena, marka, model, godiste, predjeni_kilometri, vrsta_goriva, kubikaza, snaga_motora, novo_polovno, uvoz_domace FROM vozila LIMIT $start_from, $items_per_page";
+$result = $conn->query($sql);
+
+// Dobijanje ukupnog broja vozila
+$total_sql = "SELECT COUNT(*) AS total FROM vozila";
+$total_result = $conn->query($total_sql);
+$total_row = $total_result->fetch_assoc();
+$total_items = $total_row['total'];
+$total_pages = ceil($total_items / $items_per_page);
 ?>
